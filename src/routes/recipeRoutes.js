aa2f480fe3307ab27e953cdc18f88e32a41a894c
pelatjeremy <pelat.jeremy@gmail.com>
2026-06-17 07:29:54 +0200

    feat: add recipe detail page from planning

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createCustomRecipe, searchRecipes, suggestions } from "../controllers/recipeController.js";
+import { createCustomRecipe, recipeDetail, searchRecipes, suggestions } from "../controllers/recipeController.js";
 import { requireAuth } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.get("/search", searchRecipes);
 router.get("/suggestions", requireAuth, suggestions);
+router.get("/:id", recipeDetail);
 router.post("/", requireAuth, createCustomRecipe);
 
 export default router;
